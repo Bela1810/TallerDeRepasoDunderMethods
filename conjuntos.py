cd8e7fa5eb6d9c89da79dbd1fd7ce53e34f90751
belac <belacaste3@gmail.com>
2023-03-23 20:35:45 -0500

    intersectar

--- conjuntos.py
+++ conjuntos.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 @dataclass
 
@@ -45,17 +46,24 @@ class Conjunto:
             nuevo_conjunto.agregar_elemento(objeto)
         return nuevo_conjunto
 
+    @classmethod
+    def intersectar(cls, conjunto1, conjunto2):
+        nuevo_conjunto = Conjunto(f"{conjunto1.nombre} INTERSECTADO {conjunto2.nombre}")
+        for objeto in conjunto1.lista_objetos:
+            if conjunto2.contiene(objeto):
+                nuevo_conjunto.agregar_elemento(objeto)
+        return nuevo_conjunto
 
-
-
-
-
-
-
+    def __str__(self):
+        objetos_str = ", ".join([objeto.nombre for objeto in self.lista_objetos])
+        return f"Conjunto {self.nombre}: ({objetos_str})"
 
 
 elemento_1 = Elemento("Alfa")
 elemento_2 = Elemento("Beta")
+conjunto_1 = Conjunto("A")
+conjunto_2 = Conjunto("B")
+
 
 
 
